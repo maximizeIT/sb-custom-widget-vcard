31fefb2efc4a27b51ad5efdedab225788ec49823
Max Scholz <max.scholz@maximize-it.eu>
2021-07-10 17:49:46 +0200

    initial commit

--- src/custom-widget-user-vcard.tsx
+++ src/custom-widget-user-vcard.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, ReactElement } from "react";
 import { UserInformation } from "./UserInformation";
-import { WidgetApi, SBUserProfile } from "widget-sdk";
+import { WidgetApi, SBUserProfile, ColorTheme } from "widget-sdk";
 
 import { Loading } from './Loading';
 
@@ -15,16 +15,19 @@ export interface CustomWidgetUserVcardProps {
 export const CustomWidgetUserVcard: FunctionComponent<CustomWidgetUserVcardProps> = ({ userId, widgetApi }: CustomWidgetUserVcardProps): ReactElement => {
 
   const [user, setUser] = React.useState<SBUserProfile | null>(null);
+  const [theme, setTheme] = React.useState<ColorTheme | null>(null);
   const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
+    setTheme(widgetApi.getLegacyAppTheme());
+
     widgetApi.getUserInformation(userId).then((user) => {
       setUser(user);
       setLoading(false);
     });
   }, []);
 
-  return <div>
+  return <div style={{color: theme?.colors.text}}>
     {loading && <Loading />}
     {user && <UserInformation user={user} />}
   </div>;
